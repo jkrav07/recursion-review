319d658debd90f727d8ff91ebaec52d54697415c
Julie Krav <j.krav07@gmail.com>
2023-03-27 17:47:00 -0700

    StringifyJSON done GEBCN in progress

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //console.log(className);
+  //console.log(document.body);
+  var body = document.body;
+  var result = [];
+  var traverse = function (element) {
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element);
+    }
+    //console.log(element.childNodes);
+    if (element.childNodes) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        traverse(element.childNodes[i]);
+      }
+    }
+  };
+  traverse(body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,51 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // input  object
+  // output  string
+
+
+  //if input  is string
+  // if input is a number or boolean
+  // if input is a function or undefined or null or NAN
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'function' ||
+    obj === undefined ||
+    obj === NaN) {
+    return undefined;
+  }
+
+  //if input is an arr
+  //if input is an obj
+  //declare empty array
+  //iterate through an object
+  //each key-vaule which will be written down as string, will be pushed into an array
+  //key value will be the recursive call of our function
+  if (Array.isArray(obj)) {
+    var output = [];
+
+    obj.forEach(function(ele) {
+      output.push(stringifyJSON(ele));
+    });
+    return '[' + output.join(',') + ']';
+
+  } else if (typeof obj === 'object') {
+    var output = [];
+
+    for (var key in obj) {
+      var value = stringifyJSON(obj[key]);
+
+      if (value !== undefined && key !== undefined) {
+        output.push('"' + key + '":' + value );
+      }
+
+    }
+    return '{' + output.join(',') + '}';
+  }
+
 };
